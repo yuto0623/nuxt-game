a3ed67051a29895cc70b4f01dc0b05b1129e8be0
yuto0623 <yuto.ryr0623@gmail.com>
2024-07-17 16:41:03 +0900

    update

--- components/Background.vue
+++ components/Background.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
-const { clickCount, count, timeSpeed, shopList, incrementCount, decrementCount } = useNumberStates()
+import { ref, watch } from 'vue';
+const { clickCount, count, timeSpeed, shopList, incrementCount, decrementCount } = useNumberStates();
+
+const positions = ref<{ left: number, top: number }[]>([]);
 
 function getRandomNumber() {
-  return Math.floor(Math.random() * 101);
+  return Math.floor(Math.random() * (90 - 10 + 1)) + 10;
 }
+
+watch(() => shopList.thinVillagers.count.value, (newValue) => {
+  // 増えた分だけ新しい位置を追加
+  while (positions.value.length < newValue) {
+    positions.value.push({
+      left: getRandomNumber(),
+      top: getRandomNumber()
+    });
+  }
+});
 </script>
 
-
-
 <template>
-  <NuxtImg src="/shopList/thinVillagers.png" height="40" v-for="i in shopList.thinVillagers.count.value"
-    class="absolute -z-10" :key="i" :style="{ left: `${getRandomNumber()}%`, top: `${getRandomNumber()}%` }" />
-
+  <NuxtImg src="/shopList/thinVillagers.png" height="40" v-for="(position, index) in positions" class="absolute -z-10"
+    :key="index" :style="{ left: `${position.left}%`, top: `${position.top}%` }" />
 </template>
